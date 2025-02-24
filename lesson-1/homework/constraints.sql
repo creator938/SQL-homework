e98f3dccf49f3f55c5cad13add33cdf7b513839e
creator938 <asadbekbakhromov351@gmail.com>
2025-02-24 18:37:37 +0500

    lesson1

---
+++ lesson-1/homework/constraints.sql
@@ -0,0 +1,13 @@
+create database HomeworkDB
+use HomeworkDB
+create table Students (StudentID int, FullName varchar(50), Age int, GPA decimal (3,2))
+Alter table Students 
+add Email varchar(50) 
+sp_rename 'Students.FullName', 'Name', 'COLUMN'
+Alter table Students 
+drop column Age
+
+truncate table Students 
+Select @@SERVERNAME
+Select GETDATE()
+Select * from Students
